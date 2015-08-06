c361e8b8a00684386d58fcdfb6ea83e82ec11ff1
Halvor Granskogen Bjornstad <halvorgb@gmail.com>
2015-08-06 15:53:34 +0200

    BETFOR23

--- src/HooplaTelepay/Parser/Record.hs
+++ src/HooplaTelepay/Parser/Record.hs
@@ -7,22 +7,62 @@ import           Text.ParserCombinators.Parsec
 data Record = BETFOR00 { r00_applikasjonsheader  :: Applikasjonsheader
                        , r00_foretaksnummer      :: String
                        , r00_divisjon            :: String
-                       , r00_sekvenskontrollfelt :: Int
+                       , r00_sekvenskontrollfelt :: Integer
                        , r00_produksjonsdato     :: String
                        , r00_passord             :: String
                        , r00_nytt_passord        :: String
                        , r00_operatornummer      :: String
                        , r00_egenreferanse       :: String
                        }
-            | BETFOR21 { r21_applikasjonsheader :: Applikasjonsheader }
-            | BETFOR23 { r23_applikasjonsheader :: Applikasjonsheader }
+            | BETFOR21 { r21_applikasjonsheader :: Applikasjonsheader
+                       , r21_foretaksnummer :: String
+                       , r21_kontonummer :: String
+                       , r21_sekvenskontrollfelt :: Integer
+                       , r21_referansenummer :: String
+                       , r21_betalingsdato :: String
+                       , r21_egenrefoppdrag :: String
+                       , r21_mottaker_kontonr :: String
+                       , r21_mottaker_navn :: String
+                       , r21_adresse_1 :: String
+                       , r21_adresse_2 :: String
+                       , r21_postnr :: String
+                       , r21_poststed :: String
+                       , r21_belop_til_egen_konto :: String
+                       , r21_tekstkode :: String
+                       , r21_transaksjonstype :: String
+                       , r21_slettekode :: String
+                       , r21_totalbelop :: Integer
+                       , r21_klientreferanse :: String
+                       , r21_valuteringsdato :: String
+                       , r21_valuteringmottat :: String
+                       , r21_slettearsak :: String
+                       , r21_blankettnummer :: String
+                       }
+            | BETFOR23 { r23_applikasjonsheader :: Applikasjonsheader
+                       , r23_foretaksnummer :: String
+                       , r23_kontonummer :: String
+                       , r23_sekvenskontrollfelt :: Integer
+                       , r23_referansenummer :: String
+                       , r23_melding_1 :: String
+                       , r23_melding_2 :: String
+                       , r23_melding_3 :: String
+                       , r23_kid :: String
+                       , r23_egenreffaktura :: String
+                       , r23_faktura_belop :: String
+                       , r23_debit_kredit_kode :: String
+                       , r23_fakturanummer :: String
+                       , r23_lopenummer :: String
+                       , r23_slettearsak :: String
+                       , r23_kundenummer :: String
+                       , r23_fakturadato :: String
+                       }
             | BETFOR99 { r99_applikasjonsheader  :: Applikasjonsheader
                        , r99_foretaksnummer      :: String
-                       , r99_sekvenskontrollfelt :: Int
+                       , r99_sekvenskontrollfelt :: Integer
                        , r99_produksjonsdato     :: String
-                       , r99_antall_oppdrag      :: Int
-                       , r99_totalsum_fil        :: Int
-                       , r99_antall_records      :: Int
+                       , r99_antall_oppdrag      :: Integer
+                       , r99_totalsum_fil        :: Integer
+                       , r99_antall_records      :: Integer
                        , r99_versjon_software    :: String
                        , r99_versjon_bank        :: String
                        }
@@ -70,6 +110,102 @@ parseSpecificRecord "BETFOR00" applikasjonsheader =
                        , r00_egenreferanse = egenreferanse
                        }
 
+parseSpecificRecord "BETFOR21" applikasjonsheader =
+  do foretaksnummer <- parseForetaksnummer
+     kontonummer <- replicateM 11 digit
+     sekvenskontrollfelt <- parseSekvenskontrollfelt
+     referansenummer <- replicateM 6 anyChar
+     betalingsdato  <- replicateM 6 digit
+     egenrefoppdrag <- replicateM 30 anyChar
+
+     -- reservert
+     space
+
+     mottaker_kontonr <- replicateM 11 digit
+     mottaker_navn  <- replicateM 30 anyChar
+     adresse_1 <- replicateM 30 anyChar
+     adresse_2 <- replicateM 30 anyChar
+     postnr <- replicateM 4 digit
+     poststed <- replicateM 26 anyChar
+     belop_til_egen_konto <- replicateM 15 digit
+     tekstkode <- replicateM 3 digit
+     transaksjonstype <- replicateM 1 anyChar
+     slettekode <- replicateM 1 anyChar
+     totalbelop <- fmap read $ replicateM 15 digit
+     klientreferanse <- replicateM 5 anyChar
+     valuteringsdato <- replicateM 6 digit
+     valuteringmottat <- replicateM 6 digit
+     slettearsak <- replicateM 1 anyChar
+
+     -- reservert
+     replicateM 9 space
+
+     blankettnummer <- replicateM 10 digit
+
+     return $ BETFOR21 { r21_applikasjonsheader = applikasjonsheader
+                       , r21_foretaksnummer = foretaksnummer
+                       , r21_kontonummer = kontonummer
+                       , r21_sekvenskontrollfelt = sekvenskontrollfelt
+                       , r21_referansenummer = referansenummer
+                       , r21_betalingsdato = betalingsdato
+                       , r21_egenrefoppdrag = egenrefoppdrag
+                       , r21_mottaker_kontonr = mottaker_kontonr
+                       , r21_mottaker_navn = mottaker_navn
+                       , r21_adresse_1 = adresse_1
+                       , r21_adresse_2 = adresse_2
+                       , r21_postnr = postnr
+                       , r21_poststed = poststed
+                       , r21_belop_til_egen_konto = belop_til_egen_konto
+                       , r21_tekstkode = tekstkode
+                       , r21_transaksjonstype = transaksjonstype
+                       , r21_slettekode = slettekode
+                       , r21_totalbelop = totalbelop
+                       , r21_klientreferanse = klientreferanse
+                       , r21_valuteringsdato = valuteringsdato
+                       , r21_valuteringmottat = valuteringmottat
+                       , r21_slettearsak = slettearsak
+                       , r21_blankettnummer = blankettnummer
+                       }
+
+
+parseSpecificRecord "BETFOR23" applikasjonsheader =
+  do foretaksnummer <- parseForetaksnummer
+     kontonummer    <- replicateM 11 digit
+     sekvenskontrollfelt <- parseSekvenskontrollfelt
+     referansenummer <- replicateM 6 anyChar
+     melding_1 <- replicateM 40 anyChar
+     melding_2 <- replicateM 40 anyChar
+     melding_3 <- replicateM 40 anyChar
+     kid <- replicateM 27 anyChar
+     egenreffaktura <- replicateM 30 anyChar
+     faktura_belop <- replicateM 15 digit
+     debit_kredit_kode <- replicateM 1 anyChar
+     fakturanummer <- replicateM 20 anyChar
+     lopenummer <- replicateM 3 digit
+     slettearsak <- replicateM 1 anyChar
+     kundenummer <- replicateM 15 anyChar
+     fakturadato <- replicateM 8 digit
+
+     return $ BETFOR23 { r23_applikasjonsheader = applikasjonsheader
+                       , r23_foretaksnummer = foretaksnummer
+                       , r23_kontonummer = kontonummer
+                       , r23_sekvenskontrollfelt = sekvenskontrollfelt
+                       , r23_referansenummer = referansenummer
+                       , r23_melding_1 = melding_1
+                       , r23_melding_2 = melding_2
+                       , r23_melding_3 = melding_3
+                       , r23_kid = kid
+                       , r23_egenreffaktura = egenreffaktura
+                       , r23_faktura_belop = faktura_belop
+                       , r23_debit_kredit_kode = debit_kredit_kode
+                       , r23_fakturanummer = fakturanummer
+                       , r23_lopenummer = lopenummer
+                       , r23_slettearsak = slettearsak
+                       , r23_kundenummer = kundenummer
+                       , r23_fakturadato = fakturadato
+                       }
+
+
 parseSpecificRecord "BETFOR99" applikasjonsheader =
   do foretaksnummer <- parseForetaksnummer
 
@@ -112,10 +248,13 @@ parseSpecificRecord "BETFOR99" applikasjonsheader =
                        }
 
 
+
+parseSpecificRecord _ _ = error "Unrecognized BETFOR format."
+
 parseForetaksnummer :: Parser String
 parseForetaksnummer = replicateM 11 digit
 
-parseSekvenskontrollfelt :: Parser Int
+parseSekvenskontrollfelt :: Parser Integer
 parseSekvenskontrollfelt = fmap read $ replicateM 4 digit
 
 parseProduksjonsdato :: Parser String
--- src/HooplaTelepay/Parser/Telepay.hs
+++ src/HooplaTelepay/Parser/Telepay.hs
@@ -15,6 +15,10 @@ parseTelepay :: Parser Telepay
 parseTelepay =
   do betfor00 <- parseRecord
      string "PELLEPELLEPELLEPELLE"
+     betfor21 <- parseRecord
+     string "SEPERATOR"
+     betfor23 <- parseRecord
+     string "seperator"
      betfor99 <- parseRecord
      string "pelle"
-     return $ Telepay betfor00 [] betfor99
+     return $ Telepay betfor00 [betfor21, betfor23] betfor99
